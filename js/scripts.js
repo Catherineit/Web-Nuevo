ff2cb73558205364e5f8a2d823ce03070783f3fc
Catherineit <catherine.it95@gmail.com>
2025-05-07 21:17:53 -0400

    Agregar imágenes de productos y actualizar estilos del carrusel

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,52 @@ function prevSlide() {
 setInterval(() => {
   nextSlide();
 }, 5000);
+
+
+
+
+// Carrusel "carouselProductos"
+let currentProductIndex = 0;
+const carouselProductos = document.getElementById("carouselProductos");
+const productSlides = carouselProductos.querySelectorAll(".carousel-item");
+const totalProductSlides = productSlides.length;
+
+function showProductSlide(index) {
+  if (index >= totalProductSlides) currentProductIndex = 0;
+  else if (index < 0) currentProductIndex = totalProductSlides - 1;
+  else currentProductIndex = index;
+
+  // Remover la clase "active" de todas las diapositivas
+  productSlides.forEach((slide) => slide.classList.remove("active"));
+
+  // Agregar la clase "active" a la diapositiva actual
+  productSlides[currentProductIndex].classList.add("active");
+}
+
+function nextProductSlide() {
+  showProductSlide(currentProductIndex + 1);
+}
+
+function prevProductSlide() {
+  showProductSlide(currentProductIndex - 1);
+}
+
+// Configurar los botones de navegación para "carouselProductos"
+const nextProductButton = carouselProductos.querySelector(".carousel-control-next");
+const prevProductButton = carouselProductos.querySelector(".carousel-control-prev");
+
+nextProductButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  nextProductSlide();
+});
+
+prevProductButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  prevProductSlide();
+});
+
+// Auto-slide cada 5 segundos para "carouselProductos"
+setInterval(() => {
+  nextProductSlide();
+}, 5000);
+
